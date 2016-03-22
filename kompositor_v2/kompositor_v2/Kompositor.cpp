37b231ed7ab9716dd2a50361064e954d9d5152e8
erikbefwe <erik@befwe.net>
2016-03-22 13:16:39 +0100

    Update Kompositor.cpp

--- kompositor_v2/kompositor_v2/Kompositor.cpp
+++ kompositor_v2/kompositor_v2/Kompositor.cpp
@@ -1,14 +1,41 @@
 #include "Kompositor.h"
 #include "Synthesizer.h"
+#include <qdebug.h>
 
-	Kompositor::Kompositor()
+	/*Kompositor::Kompositor(QWidget *parent) : QMainWindow
 	{
-		notTypVal = 0;
+	
 	}
 
-	Kompositor::Kompositor(int notTyp)
-	{
-		notTypVal = notTyp;
-	}
+	*/
 
+
+
+void Kompositor::mousePressEvent(QMouseEvent *e)
+{
+	int hojd = pixelToHöjd(e->y());
+}
+
+
+
+
+int Kompositor::pixelToHöjd(int ypixel)
+{
+	float enivå = 413;
+	float step = 8;
+	int höjd = -1;
+	for (int i = 0; i < 12; i++)
+	{
+		if (ypixel <= (enivå + (2 - i)*step + step / 2) && ypixel >(enivå + (2 - i)*step - step / 2))
+			höjd = i;
+	}
+	qDebug() << höjd;
+	return höjd;
+}
+
+
+
+
+	void ritaOm();
+	void paintEvent();
 
