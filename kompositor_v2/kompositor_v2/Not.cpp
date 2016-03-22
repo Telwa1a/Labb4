6809968fb6f0314caff351a2622407cc76b087aa
erikbefwe <erik@befwe.net>
2016-03-22 10:26:10 +0100

    Create Not.cpp

---
+++ kompositor_v2/kompositor_v2/Not.cpp
@@ -0,0 +1,17 @@
+#include "Not.h"
+
+
+
+Not::Not(int längd, int höjd)
+{
+	längd = 0;
+	höjd = 0;
+}
+
+
+int skrivUt(int xPos);
+int skrivUtC(int xPos);
+void spelaUpp(Synthesizer);
+
+
+//testtesttest
