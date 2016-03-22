8bf1a50a544f1d72d0a5cd155d8fb5030b25356a
erikbefwe <erik@befwe.net>
2016-03-22 13:17:22 +0100

    Update Kompositor.h

--- kompositor_v2/kompositor_v2/Kompositor.h
+++ kompositor_v2/kompositor_v2/Kompositor.h
@@ -1,30 +1,61 @@
 #pragma once
 
+#ifndef KOMPOSITOR_H
+#define KOMPOSITOR_H
+
+#include <QtWidgets/QMainWindow>
+#include <QtGui>
+#include "ui_kompositor_v2.h"
+#include "Notblad.h"
+#include <QtWidgets/QMainWindow>
+#include <QtGui>
+#include "ui_kompositor_v2.h"
+#include "Notblad.h"
+#include "Synthesizer.h"
+
+
 	/**
 	* A class representing a point in a three-dimensional plane.
 	*
 	* This class represents a point in a three-dimensional plane with an x-axes,
 	* y-axes and z-axes.
 	*/
-	class Kompositor
+class Kompositor : public QMainWindow
 	{
 
 	public:
 	
-		Kompositor();
-		Kompositor(int notTyp);
+		Kompositor(QWidget *parent = 0);
+	
 
 		int notTypVal;
 
-		void mousePressEvent();
-		void pixelToHöjd();
+		void mousePressEvent(QMouseEvent *e);
+
+
+
+
+		int pixelToHÃ¶jd(int ypixel);
+
+
+
+
+
+
 		void ritaOm();
-		void fjärdedelsnotKlickad();
+		void fjÃ¤rdedelsnotKlickad();
 		void paintEvent();
 
-	//private:
+	private:
+		Ui::kompositor_v2Class ui;
+
+	
+		Notblad _notblad;
 	
 	};
+
+#endif // KOMPOSITOR_H
+
 
 
 
